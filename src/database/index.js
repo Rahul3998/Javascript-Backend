c2329f1db403d3a56385f11549321f1f628e405d
Rahul Adagale <137898360+Rahul3998@users.noreply.github.com>
2025-03-02 20:45:51 +0530

    database configuration added

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,34 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import mysql from "mysql2";
+
+// Create connection configuration
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, // Your MySQL host
+  user: process.env.DB_USER, // Your MySQL username
+  password: process.env.DB_PASSWORD, // Your MySQL password
+  database: process.env.DB_NAME, // Your database name
+});
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
-    console.log(
-      `Connection Successfull..! Connection Host :: ${connectionInstance.connection.host}`
-    );
+    // Connection Using Local Database
+    const connectionInstance = await connection.connect((error) => {
+      if (error) {
+        console.log("Connection Error :: ", error);
+      } else {
+        console.log("Connection Successful");
+      }
+      connection.end();
+    });
+
+    // Connection using MongoDB
+    // const connectionInstance = await mongoose.connect(
+    //   `${process.env.MONGODB_URI}/${DB_NAME}`
+    // );
+    // console.log(
+    //   `Connection Successfull..! Connection Host :: ${connectionInstance.connection.host}`
+    // );
   } catch (error) {
     console.log("CONNECTION ERROR :: ", error);
     process.exit(1);
